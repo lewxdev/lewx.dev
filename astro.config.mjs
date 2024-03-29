f893627ab25b5ffb5930b30e861c4d287882bfc9
J. Lewis <6710419+lewxdev@users.noreply.github.com>
2024-03-29 15:04:56 -0400

    chore(setup): configure tailwind integration
    follow the guide to configure tailwindcss with astro
    (https://docs.astro.build/guides/integrations-guide/tailwind)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // learn more: https://astro.build/config
 
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+});
